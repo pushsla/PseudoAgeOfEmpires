2b0a057f552baffcc6d9836ee5bd5594ab23fc30
pushsla <push2001sla@gmail.com>
2020-10-10 23:36:37 +0300

    Better AI reaction }=)

--- cs.2020-10-07-struct/Config.cs
+++ cs.2020-10-07-struct/Config.cs
@@ -12,7 +12,7 @@ namespace cs._2020_10_07_struct
         public static readonly int GamerBaseHp = 100;
         public static readonly int GamerBaseSpeed = 15;
         public static readonly int GamerShootCooldown = 800;
-        public static readonly int AIReactionDelay = 800;
+        public static readonly int AIReactionDelay = 200;
 
         public static readonly int CollectableSpawnInterval = 5000;
         public static readonly int MaxCollectables = 5;
